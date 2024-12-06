d8e1d84564faa978414f15ce3592c82594c8dc37
Koushik Sarkar <koushiksarkar@Koushiks-MacBook-Pro.local>
2024-12-06 20:08:12 +0530

    removed wrapper

--- agent.py
+++ agent.py
@@ -23,14 +23,7 @@ def python_repl(
     result_str = f"Code output: {result}"
     return result_str
 
-llm_research = ChatOpenAI(
-    model="gpt-4o-mini",
-    temperature=0,
-    max_tokens=None,
-    timeout=None,
-    max_retries=2
-)
-llm_chart = ChatOpenAI(
+llm_generic = ChatOpenAI(
     model="gpt-4o-mini",
     temperature=0,
     max_tokens=None,
@@ -39,13 +32,13 @@ llm_chart = ChatOpenAI(
 )
 
 research_agent = create_react_agent(
-    llm_research,
+    llm_generic,
     tools=[DuckDuckGoSearchRun()],
     state_modifier="You are best at researching a topic. You should do a thorough research. Your output will be used by a chart generator agent to visually display the data. Hence you should provide accurate data. Also specify the chart types like barchart, pie chart etc. that will effectively display the data. The chart generator may ask for more information, so be prepared to do further research and provide it."
 )
 
 chart_agent = create_react_agent(
-    llm_chart,
+    llm_generic,
     tools=[python_repl],
     state_modifier="""Take the data and chart specifications provided by the researcher agent, and write Python code to generate the requested chart. If the provided data is insufficient to generate the chart, ask for the missing details, being specific about what is needed. Do not ask for information that is already provided. You must ask for clarification or additional data at least once. When asking any question to the researcher, include the phrase QUESTION_TO_RESEARCHER in your response, otherwise the researcher will not answer.
 
@@ -55,19 +48,9 @@ chart_agent = create_react_agent(
 class GraphState(TypedDict):
     messages: Annotated[Sequence[BaseMessage], add_messages]
 
-def research_node(state: GraphState) -> GraphState:
-    returned_state = research_agent.invoke(state)
-    messages = returned_state["messages"]
-    return {"messages": AIMessage(content=messages[-1].content, name="research_agent")}
-
-def chart_node(state: GraphState) -> GraphState:
-    returned_state = chart_agent.invoke(state)
-    messages = returned_state["messages"]
-    return {"messages": AIMessage(content=messages[-1].content, name="chart_agent")}
-
 graph_builder = StateGraph(GraphState)
-graph_builder.add_node("research_node", research_node)
-graph_builder.add_node("chart_node", chart_node)
+graph_builder.add_node("research_node", research_agent)
+graph_builder.add_node("chart_node", chart_agent)
 
 graph_builder.add_edge(START, "research_node")
 graph_builder.add_edge("research_node", "chart_node")
